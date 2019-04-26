b585e573a6aabbd1adbbb523a2af3ae6777a8ecd
Matt Gibney <moxyfruzy@googlemail.com>
2019-04-26 20:53:44 +0100

    working tests using z80 emulation

--- test/main.spec.js
+++ test/main.spec.js
@@ -1,9 +1,49 @@
-var assert = require('assert');
+/* eslint-env node, mocha */
+const assert = require('chai').assert;
+var fs = require('fs');
+
+const { z80, memory } = require('z80testsuite');
+
+const binary = fs.readFileSync('./bin/main.bin', 'hex');
+let program = binary.match(/.{2}/g);
+program = program.map(byte => parseInt(`0x${byte}`));
+
+const memortStart = 0;
+let memoryPos = memortStart;
+program.forEach(byte => {
+  memory.memoryObj[memoryPos] = byte;
+  memoryPos = memoryPos + 1;
+});
 
 describe('Array', function() {
   describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
+    before(function() {
+      this.memory = memory;
+      this.z80 = z80;
+    });
+    it('should count up', function() {
+      z80.run_instruction();
+      assert.equal(this.z80.getState().a, 0);
+
+      z80.run_instruction();
+      z80.run_instruction();
+      assert.equal(this.z80.getState().a, 2);
+
+      z80.run_instruction();
+      z80.run_instruction();
+      assert.equal(this.z80.getState().a, 4);
+
+      z80.run_instruction();
+      z80.run_instruction();
+      assert.equal(this.z80.getState().a, 6);
+
+      z80.run_instruction();
+      z80.run_instruction();
+      assert.equal(this.z80.getState().a, 8);
+
+      z80.run_instruction();
+      z80.run_instruction();
+      assert.equal(this.z80.getState().a, 10);
     });
   });
 }); 
